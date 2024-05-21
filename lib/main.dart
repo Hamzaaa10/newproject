67e51b28b5fa59ce0a8d8f7e93bdf820a71344c6
husseink7aled <kh7299460@gmail.com>
2024-05-21 21:58:31 +0300

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-import 'dart:js_interop';
 import 'package:auth/core/my_theme.dart';
 import 'package:auth/dashboard/view/dashboard_page.dart';
 import 'package:auth/firebase_options.dart';
@@ -8,25 +6,22 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main()async {
-  
-  
-
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name: "dashboard2-a3fdd",
-    
-    options: DefaultFirebaseOptions.currentPlatform);
-  
-    final shared=await SharedPreferences.getInstance() ;
-     
-  bool onbording=shared.getBool("onbording")??false;
-  runApp( MyApp(onbording: onbording,));
-  
+      name: "dashboard2-a3fdd",
+      options: DefaultFirebaseOptions.currentPlatform);
 
+  final shared = await SharedPreferences.getInstance();
+
+  bool onbording = shared.getBool("onbording") ?? false;
+  runApp(MyApp(
+    onbording: onbording,
+  ));
 }
+
 class MyApp extends StatelessWidget {
-   MyApp({super.key,required this.onbording});
+  MyApp({super.key, required this.onbording});
   final bool onbording;
 
   @override
@@ -34,9 +29,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: MyTheme.instance.light,
-      darkTheme:MyTheme.instance.dark ,
-      themeMode:ThemeMode.dark ,
-      home:dashboard_page() ,
+      darkTheme: MyTheme.instance.dark,
+      themeMode: ThemeMode.dark,
+      home: dashboard_page(),
 
       // onbording?Signin():onbordingpage(),
     );
