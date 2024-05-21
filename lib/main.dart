14b62bef5929cc79fcc53f76ecc997250d06bef3
husseink7aled <kh7299460@gmail.com>
2024-05-21 22:19:19 +0300

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:auth/core/my_theme.dart';
 import 'package:auth/dashboard/view/dashboard_page.dart';
 import 'package:auth/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -27,13 +27,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: MyTheme.instance.light,
-      darkTheme: MyTheme.instance.dark,
-      themeMode: ThemeMode.dark,
-      home: dashboard_page(),
+        debugShowCheckedModeBanner: false,
+        theme: MyTheme.instance.light,
+        darkTheme: MyTheme.instance.dark,
+        themeMode: ThemeMode.dark,
+        home: AnimatedSplashScreen(
+            duration: 3000,
+            splash: Icon(
+              Icons.shopping_cart_sharp,
+              size: 200,
+            ),
+            nextScreen: dashboard_page(),
+            splashTransition: SplashTransition.fadeTransition,
+            backgroundColor: Colors.blue));
 
-      // onbording?Signin():onbordingpage(),
-    );
+    // onbording?Signin():onbordingpage(),
   }
 }
